d8eb12b9de5a88587a6808bd18c92b3722e1fd42
datadiego <juandiegomariscal@gmail.com>
2025-05-20 12:31:28 +0200

    test linter

--- build.js
+++ build.js
@@ -1,31 +1,31 @@
-import {menu} from './menu.js';
-import nunjucks from 'nunjucks';
-import fs from 'fs';
-import { createHTMLFile } from './scripts/generateHtml.js';
+import { menu } from './menu.js'
+import nunjucks from 'nunjucks'
+import fs from 'fs'
+import { createHTMLFile } from './scripts/generateHtml.js'
 
 if (!fs.existsSync('./dist')) {
-    fs.mkdirSync('./dist');
+  fs.mkdirSync('./dist')
 }
 
 if (!fs.existsSync('./dist/imgs')) {
-    fs.mkdirSync('./dist/imgs');
+  fs.mkdirSync('./dist/imgs')
 }
-const imgsDir = './imgs';
-const outputImgsDir = './dist/imgs';
+const imgsDir = './imgs'
+const outputImgsDir = './dist/imgs'
 fs.readdirSync(imgsDir).forEach(file => {
-    const srcPath = `${imgsDir}/${file}`;
-    const destPath = `${outputImgsDir}/${file}`;
-    fs.copyFileSync(srcPath, destPath);
-});
+  const srcPath = `${imgsDir}/${file}`
+  const destPath = `${outputImgsDir}/${file}`
+  fs.copyFileSync(srcPath, destPath)
+})
 
-const menuTemplate = './views/menu.njk';
-const aboutTemplate = './views/about.njk';
-nunjucks.configure("views",{ autoescape: true });
+const menuTemplate = './views/menu.njk'
+const aboutTemplate = './views/about.njk'
+nunjucks.configure('views', { autoescape: true })
 
-createHTMLFile(menuTemplate, { menu }, './dist/index.html');
-createHTMLFile(menuTemplate, { menu }, './dist/menu.html');
-createHTMLFile(aboutTemplate, {}, './dist/about.html');
+createHTMLFile(menuTemplate, { menu }, './dist/index.html')
+createHTMLFile(menuTemplate, { menu }, './dist/menu.html')
+createHTMLFile(aboutTemplate, {}, './dist/about.html')
 
-const cssFile = 'style.css';
-const outputCssFile = './dist/style.css';
-fs.copyFileSync(cssFile, outputCssFile);
+const cssFile = 'style.css'
+const outputCssFile = './dist/style.css'
+fs.copyFileSync(cssFile, outputCssFile)
--- scripts/generateHtml.js
+++ scripts/generateHtml.js
@@ -1,13 +1,13 @@
-import fs from 'fs';
-import nunjucks from 'nunjucks';
-export function generateHTML(template, data) {
-    const templateContent = fs.readFileSync(template, 'utf-8');
-    const html = nunjucks.renderString(templateContent, data);
-    return html;
+import fs from 'fs'
+import nunjucks from 'nunjucks'
+export function generateHTML (template, data) {
+  const templateContent = fs.readFileSync(template, 'utf-8')
+  const html = nunjucks.renderString(templateContent, data)
+  return html
 }
 
-export function createHTMLFile(template, data, outputFile) {
-    const html = generateHTML(template, data);
-    fs.writeFileSync(outputFile, html, 'utf-8');
-    console.log(`Archivo ${outputFile} generado con éxito.`);
+export function createHTMLFile (template, data, outputFile) {
+  const html = generateHTML(template, data)
+  fs.writeFileSync(outputFile, html, 'utf-8')
+  console.log(`Archivo ${outputFile} generado con éxito.`)
 }
